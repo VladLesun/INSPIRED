94f879af86c34f5ab5496d39987ee61d09f4612f
Vlad Lesun <87444342+VladLesun@users.noreply.github.com>
2025-08-14 22:51:25 +0300

    feat: update API_URL

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,6 +1,6 @@
 export const DATA = {};
 export const COUNT_PAGINATION = 3;
-export const API_URL = 'https://pie-happy-currency.glitch.me';
+export const API_URL = 'https://api-inspired.onrender.com';
 export const TITLE = {
 	women: {
 		title: 'Новая коллекция Бюстгальтер-балконет',
